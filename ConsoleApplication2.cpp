241b9595dbb379d0ac1e537f5198e4897b215791
С - 8 < - 8@DESKTOP-291QIPP>
2024-06-19 18:40:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,40 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+	setlocale(LC_ALL, "RU");
+	int i = 0;
+    vector<int> vec1(5);
+	vector<int> vec2(5);
+	for (int i = 0; i < 5; i++)
+	{
+		vec1[i] = -20 + rand() % 40;
+		cout << vec1[i] << " ";
+	}
+	cout << "\n";
+
+	for (int elem : vec1)
+	{
+		if (elem <= 0){}
+		else
+		{ 
+			vec2[i] = elem; 
+			i++;
+		}
+	}
+	for (int elem : vec2)
+	{
+		if (elem == 0)
+		{
+
+		}
+		else
+		{
+			cout << elem << " ";
+		}
+	}
+}
+
